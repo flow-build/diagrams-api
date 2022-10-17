2f24f879144ea3dda4ea648f61c8d8a06b365101
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-10-17 20:31:31 -0300

    feat: add new classes on knex persist

--- src/persist/knex.js
+++ src/persist/knex.js
@@ -1,4 +1,7 @@
 const { Diagram } = require('../entities/diagram');
+const { Blueprint } = require('../entities/blueprint');
+const { Workflow } = require('../entities/workflow');
+const { DiagramToWorkflow } = require('../entities/diagramToWorkflow');
 
 class KnexPersist {
   constructor(db, class_, table) {
@@ -43,24 +46,81 @@ class DiagramKnexPersist extends KnexPersist {
   }
   
   async getByWorkflowId(workflow_id) {
-    return await this._db(this._table).select('*').where('workflow_id', workflow_id)
+    return await this._db(this._table)
+      .select('id', 'name', 'diagram_xml', 'blueprint_id','user_id', 'created_at', 
+        'updated_at', 'aligned', 'workflow_id')
+      .join('diagram_to_workflow', 'diagram_to_workflow.diagram_id', `${this._table}.id`)
+      .where('workflow_id', workflow_id)
       .orderBy('updated_at', 'desc');
   }
   
   async getLatestByWorkflowId(workflow_id) {
-    return await this._db(this._table).select('*').where('workflow_id', workflow_id)
+    return await this._db(this._table)
+      .select('id', 'name', 'diagram_xml', 'blueprint_id','user_id', 'created_at', 
+        'updated_at', 'aligned', 'workflow_id')
+      .join('diagram_to_workflow', 'diagram_to_workflow.diagram_id', `${this._table}.id`)
+      .where('workflow_id', workflow_id)
       .orderBy('updated_at', 'desc')
       .first();
   }
   
   async getByUserAndWF(user_id, workflow_id) {
-    return await this._db(this._table).select('*')
+    return await this._db(this._table)
+      .select('id', 'name', 'diagram_xml', 'blueprint_id','user_id', 'created_at', 
+        'updated_at', 'aligned', 'workflow_id')
+      .join('diagram_to_workflow', 'diagram_to_workflow.diagram_id', `${this._table}.id`)
       .where('workflow_id', workflow_id)
       .andWhere('user_id', user_id)
       .orderBy('updated_at', 'desc');
   }
 }
 
+class BlueprintKnexPersist extends KnexPersist {
+  constructor(db) {
+    super(db, Blueprint, 'blueprints');
+  }
+
+  async save(blueprint) {
+    await this._db(this._table).insert(blueprint);
+    return 'create';
+  }
+}
+
+class WorkflowKnexPersist extends KnexPersist {
+  constructor(db) {
+    super(db, Workflow, 'workflows');
+  }
+
+  async save(workflow) {
+    await this._db(this._table).insert(workflow);
+    return 'create';
+  }
+}
+
+class DiagramToWorkflowKnexPersist extends KnexPersist {
+  constructor(db) {
+    super(db, DiagramToWorkflow, 'diagram_to_workflow');
+  }
+
+  async save(workflow) {
+    await this._db(this._table).insert(workflow);
+    return 'create';
+  }
+
+  async getDiagramIdByWorkflowId(workflow_id) {
+    return await this._db(this._table).select('*').where('workflow_id', workflow_id)
+      .first();
+  }
+
+  async getWorkflowIdByDiagramId(diagram_id) {
+    return await this._db(this._table).select('*').where('diagram_id', diagram_id)
+      .first();
+  }
+}
+
 module.exports = {
-  DiagramKnexPersist
+  DiagramKnexPersist,
+  BlueprintKnexPersist,
+  WorkflowKnexPersist,
+  DiagramToWorkflowKnexPersist
 }
