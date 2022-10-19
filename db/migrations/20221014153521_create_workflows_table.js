27691731ba801578063f9ff761cf1509fc4f4286
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-10-19 13:10:28 -0300

    fix: adjust migrations and seeds

--- db/migrations/20221014153521_create_workflows_table.js
+++ db/migrations/20221014153521_create_workflows_table.js
@@ -3,7 +3,6 @@ exports.up = function (knex) {
     table.uuid("id").primary();
     table.string("server", 255).notNullable();
     table.uuid("blueprint_id").notNullable();
-    table.foreign("id").references("diagram_to_workflow.workflow_id");
     table.foreign("blueprint_id").references("blueprints.id");
   });
 };
--- db/migrations/20221014154948_create_diagram_to_workflow_table.js
+++ db/migrations/20221014154948_create_diagram_to_workflow_table.js
@@ -1,7 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable("diagram_to_workflow", (table) => {
     table.uuid("diagram_id").notNullable().unique();
-    table.uuid("workflow_id").notNullable().unique();
+    table.uuid("workflow_id").notNullable();
+    table.foreign("diagram_id").references("diagrams.id");
+    table.foreign("workflow_id").references("workflows.id");
   });
 };
 
