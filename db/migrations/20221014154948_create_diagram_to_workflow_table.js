b4945501c4f4ccd701d319f805e0a1d8f143f5b5
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-10-28 15:30:42 -0300

    build: update migrations

--- db/migrations/20221014154948_create_diagram_to_workflow_table.js
+++ db/migrations/20221014154948_create_diagram_to_workflow_table.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("diagram_to_workflow", (table) => {
-    table.uuid("diagram_id").notNullable()
+    table.uuid("diagram_id").notNullable();
     table.uuid("workflow_id").notNullable();
     table.unique(["diagram_id", "workflow_id"]);
     table.foreign("diagram_id").references("diagrams.id");
