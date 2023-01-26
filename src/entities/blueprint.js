3f68c0ba5aa2129f6b676829b418e7b3f9e49be1
Gustavo Haramura <gustavo.haramura@fdte.io>
2023-01-26 17:24:54 -0300

    feat: verify whether the blueprint exists before saving

--- src/entities/blueprint.js
+++ src/entities/blueprint.js
@@ -1,6 +1,29 @@
-const { v4: uuid } = require('uuid');
-const { PersistedEntity } = require('./base');
-const _ = require('lodash');
+const { v4: uuid } = require("uuid");
+const { PersistedEntity } = require("./base");
+const _ = require("lodash");
+
+const sortById = (array) => {
+  return array.sort((a, b) => {
+    let x = a.id.toLowerCase();
+    let y = b.id.toLowerCase();
+    if (x > y) {
+      return 1;
+    }
+    if (x < y) {
+      return -1;
+    }
+    return 0;
+  })
+};
+
+const organize = (spec) => {
+  const lanes = sortById(spec.lanes);
+  const nodes = sortById(spec.nodes);
+  return {
+    lanes,
+    nodes,
+  };
+}
 
 class Blueprint extends PersistedEntity {
   static getEntityClass() {
@@ -11,7 +34,7 @@ class Blueprint extends PersistedEntity {
     if (!serialized) {
       return;
     }
-    
+
     if (_.isArray(serialized)) {
       return serialized.map((data) => this._deserialized(data));
     } else {
@@ -22,26 +45,28 @@ class Blueprint extends PersistedEntity {
   static _deserialized(data) {
     return {
       id: data.id,
-      blueprint_spec: data.blueprint_spec
-    }
+      blueprint_spec: data.blueprint_spec,
+    };
   }
 
   static serialize(blueprint) {
     return {
       id: blueprint.id,
-      blueprint_spec: blueprint.blueprint_spec
-    }
+      blueprint_spec: organize(blueprint.blueprint_spec),
+    };
   }
 
   constructor(blueprint_spec) {
     super();
-
     this.id = uuid();
-    this.blueprint_spec = blueprint_spec;
+    this.blueprint_spec = organize(blueprint_spec);
   }
 
+  async save(...args) {
+    return await this.getPersist().save(this.serialize(), ...args);
+  }
 }
 
 module.exports = {
-  Blueprint
-}
+  Blueprint,
+};
